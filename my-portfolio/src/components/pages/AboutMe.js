4993c2be80b4d49cbfbde146faa37dbb211f034e
Andréia Ribeiro <asribeirodesign@gmail.com>
2022-01-22 14:36:55 +0000

    About me
    Fixing css

--- my-portfolio/src/components/pages/AboutMe.js
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -7,7 +7,7 @@ import '../../App.css';
 function AboutMe() {
   window.scrollTo(0, 0)
   return (
-    <div className='main-container'>
+    <div className='main-container-2'>
       <video src='/videos/video-3.mp4' autoPlay loop muted />
       <h1>ANDREIA RIBEIRO</h1>
       <div className="main-banner">
