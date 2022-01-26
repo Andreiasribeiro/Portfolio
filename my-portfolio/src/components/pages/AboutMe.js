4a33dbd9d1813e2b3f68637e15ad5823c5160936
Andréia Ribeiro <asribeirodesign@gmail.com>
2022-01-26 23:15:13 +0000

    Background

--- my-portfolio/src/components/pages/AboutMe.js
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe() {
   window.scrollTo(0, 0)
   return (
     <div className='main-container'>
-      <video src='/videos/video-3.mp4' autoPlay loop muted />
+      <video src='/videos/video-3.mp4' playsInline autoPlay loop muted />
       <h1>ANDREIA RIBEIRO</h1>
       <div className="main-banner">
         <div className='banner-left'>
