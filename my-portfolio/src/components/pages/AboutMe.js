c9d8cd18ba8df621d9ed9837ac7aa08852877f95
Andréia Ribeiro <74618504+Andreiasribeiro@users.noreply.github.com>
2024-09-01 18:21:25 +0100

    Update AboutMe.js

--- my-portfolio/src/components/pages/AboutMe.js
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -17,8 +17,8 @@ function AboutMe() {
         </div>
         <div className='banner-right'>
 
-          <p>Hello! <br></br> I am a passionate Web Development enthusiast with a BSc in Industrial Design and a Higher Diploma in Science in Computing. With over 10 years of experience in product design, graphic design, and illustration for various B2B and B2C clients across different industries, I bring design concepts to life using HTML, CSS, and JavaScript, effectively translating UX ideas into functional code.
-My educational background, which combines design and technical expertise, has given me a unique perspective on product usability and functionality. I also have a deep passion for the arts, often expressing my creativity through drawing and painting.
+          <p>Hello! <br></br> I am a passionate Web Development and Quality Assurance enthusiast with a BSc in Industrial Design and a Higher Diploma in Science in Computing. With over ten years of experience in product design, graphic design, and illustration for various B2B and B2C clients across diverse industries, I specialize in bringing design concepts to life using HTML, CSS, and JavaScript.
+<br></br> My educational background, which blends design and technical expertise, provides me with a unique perspective on product usability and functionality. Currently, I work as a Tech Support Engineer, where I am dedicated to learning and applying QA methodologies, tools, and best practices. In addition to my technical work, I have a strong passion for the arts, channeling my creativity through drawing and painting.
 <br></br> Feel free to explore my website to learn more about my work and skills. If you’d like more information or wish to connect, don’t hesitate to reach out.</p>
         </div>
       </div>
