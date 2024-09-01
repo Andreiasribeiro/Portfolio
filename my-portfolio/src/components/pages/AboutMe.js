4c1e0d1c1f8264a4f25f5290efcb54417f4ebc14
Andréia Ribeiro <74618504+Andreiasribeiro@users.noreply.github.com>
2024-09-01 17:47:17 +0100

    Update AboutMe.js

--- my-portfolio/src/components/pages/AboutMe.js
+++ my-portfolio/src/components/pages/AboutMe.js
@@ -17,11 +17,9 @@ function AboutMe() {
         </div>
         <div className='banner-right'>
 
-          <p>Hello! <br></br> I am a UI designer and frontend web developer holding a BSc in Industrial Design, Higher Diploma in Science in Computing,
-            with overall 10 years of experience in product design, and graphic & illustration design for several B2B and B2C customers in different sectors and industries.
-            I bring the design concept to life through a mix of HTML, CSS, and JavaScript, translating UX concepts into code.
-            I am also passionate about the arts, and I like drawing and painting to express my creativity. <br></br>
-            Welcome to explore this website and know more about my work and skills. Feel free to contact me if you want to obtain more information.</p>
+          <p>Hello! <br></br> I am a passionate Web Development enthusiast with a BSc in Industrial Design and a Higher Diploma in Science in Computing. With over 10 years of experience in product design, graphic design, and illustration for various B2B and B2C clients across different industries, I bring design concepts to life using HTML, CSS, and JavaScript, effectively translating UX ideas into functional code.
+My educational background, which combines design and technical expertise, has given me a unique perspective on product usability and functionality. I also have a deep passion for the arts, often expressing my creativity through drawing and painting.
+<br></br> Feel free to explore my website to learn more about my work and skills. If you’d like more information or wish to connect, don’t hesitate to reach out.</p>
         </div>
       </div>
     </div>
